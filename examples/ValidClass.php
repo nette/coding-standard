53b7933471c0384b6e1a59b997c76960e8a7470b
TomasVotruba <tomas.vot@gmail.com>
2017-05-16 17:11:07 +0200

    [example] create messy element order

--- examples/ValidClass.php
+++ examples/ValidClass.php
@@ -9,13 +9,46 @@ use stdClass;
 
 class ValidClass
 {
+	protected $listOfSkills = [
+		'empathy',
+		'respect',
+	];
+
+	private const DREAM_COUNT = 250;
+
+	public $listOfEmotions = [
+		'love',
+		'happiness',
+	];
+
+	public const JOY_COUNT = 5;
+
 	private $listOfElements = [
 		'Nette',
 		'Latte',
 	];
 
+	private function internalMethod()
+	{
+	}
+
+	protected const CHILD_COUNT = 1;
+
+	public function __destruct()
+	{
+	}
+
+	protected function anotherMethod()
+	{
+	}
+
+
 	public function validMethod()
 	{
 		return TRUE;
+	}
+
+	public function __construct()
+	{
 	}
 }
