62bd0d74d82703dd0f6d078ecb8dd02648eac76d
TomasVotruba <tomas.vot@gmail.com>
2017-07-06 15:04:33 +0200

    [examples] drop unchecked param types

--- examples/ValidClass.php
+++ examples/ValidClass.php
@@ -42,7 +42,7 @@ class ValidClass
 		return TRUE;
 	}
 
-	protected function anotherMethod(string $someArgument, int $anotherArgument)
+	protected function anotherMethod($someArgument, $anotherArgument)
 	{
 		$sum = $someArgument + $anotherArgument;
 		$sum += 5;
