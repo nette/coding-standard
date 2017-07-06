827eab49784ba91e9e1389cd5103f4f4f963ddcf
TomasVotruba <tomas.vot@gmail.com>
2017-07-06 17:08:15 +0200

    [Fixer] own version of VisibilityRequiredFixer added: ClassAndTraitVisibilityRequiredFixer [closes #9]

--- examples/ValidInterface.php
+++ examples/ValidInterface.php
@@ -2,53 +2,7 @@
 
 namespace Nette\CodingStandard\Examples;
 
-use Alphabetcial;
-use DateTimeInterface;
-use stdClass;
-
-class ValidClass
+interface ValidInterface
 {
-	public const JOY_COUNT = 5;
-
-	protected const CHILD_COUNT = 1;
-
-	private const DREAM_COUNT = 250;
-
-	public $listOfEmotions = [
-		'love',
-		'happiness',
-	];
-
-	protected $listOfSkills = [
-		'empathy',
-		'respect',
-	];
-
-	private $listOfElements = [
-		'Nette',
-		'Latte',
-	];
-
-	public function __construct()
-	{
-	}
-
-	public function __destruct()
-	{
-	}
-
-	public function validMethod()
-	{
-		return TRUE;
-	}
-
-	protected function anotherMethod($someArgument, $anotherArgument)
-	{
-		$sum = $someArgument + $anotherArgument;
-		$sum += 5;
-	}
-
-	private function internalMethod()
-	{
-	}
+	function interfaceMethod();
 }
---
+++ src/Fixer/ClassNotation/ClassAndTraitVisibilityRequiredFixer.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace Nette\CodingStandard\Fixer\ClassNotation;
+
+use PhpCsFixer\AbstractFixer;
+use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
+use PhpCsFixer\Fixer\ConfigurationDefinitionFixerInterface;
+use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
+use PhpCsFixer\Tokenizer\Tokens;
+use ReflectionMethod;
+use SplFileInfo;
+
+final class ClassAndTraitVisibilityRequiredFixer extends AbstractFixer implements ConfigurationDefinitionFixerInterface
+{
+    /**
+     * @var VisibilityRequiredFixer
+     */
+    private $visibilityRequiredFixer;
+
+    public function __construct()
+    {
+        $this->visibilityRequiredFixer = new VisibilityRequiredFixer;
+        parent::__construct();
+    }
+
+    public function isCandidate(Tokens $tokens): bool
+    {
+        return $tokens->isAnyTokenKindsFound([T_CLASS, T_TRAIT]);
+    }
+
+    public function getDefinition(): FixerDefinitionInterface
+    {
+        return $this->visibilityRequiredFixer->getDefinition();
+    }
+
+    public function configure(array $configuration = null): void
+    {
+        $this->visibilityRequiredFixer->configure($configuration);
+    }
+
+    protected function applyFix(SplFileInfo $file, Tokens $tokens): void
+    {
+        /**
+         * Hack note: This reflection opening was chosen as more future-proof
+         * than duplicating whole 300-lines class. As "VisibilityRequiredFixer" class is final
+         * and "applyFix()" is final, there is no other way round it.
+         */
+        $method = new ReflectionMethod($this->visibilityRequiredFixer, 'applyFix');
+        $method->setAccessible(true);
+        $method->invoke($this->visibilityRequiredFixer, $file, $tokens);
+    }
+}
