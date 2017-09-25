8f534b725e1b268822845af07c471435eb9155b3
TomasVotruba <tomas.vot@gmail.com>
2017-09-25 22:23:11 +0200

    add test data for PR #16

---
+++ pull-request-16/MultiExceptionFile.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Nette\CodingStandard\Examples;
+
+
+class MultiExceptionFile
+{
+	public $property;
+
+
+}
+
+
+class AnotherMultiExceptionFile
+{
+	public $property;
+
+
+}
