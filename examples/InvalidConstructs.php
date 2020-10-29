aa673bc089db81081596ae9da8868ce83e1b4cc0
David Grudl <david@grudl.com>
2020-10-29 10:21:50 +0100

    added examples

---
+++ examples/InvalidConstructs.php
@@ -0,0 +1,32 @@
+<?php
+
+declare ( strict_types = 1 ) ;
+
+
+function  test ( ? array  $a , int$b) {
+return 1;
+}
+
+function & test2 () : int
+{
+return 1;
+}
+
+$a=(int) $b;;$x=2;
+
+$a = [ 1 , 2 ,3, ];
+$a = [
+		1 ,
+2 ,
+	3
+	];
+
+
+[TRUE, False, NULL];
+
+trim ( 'a' ,$b ) ;
+
+trim(
+		1,
+2
+);
