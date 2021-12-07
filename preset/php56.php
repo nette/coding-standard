cc86a83908eafceae90b8697ebb5f24f25efaa09
David <david@grudl.com>
2021-12-08 00:19:43 +0100

    removed presets

--- preset/php56.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return function (Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator $containerConfigurator): void {
-	$containerConfigurator->import(__DIR__ . '/common/*.php');
-
-	$services = $containerConfigurator->services();
-};
