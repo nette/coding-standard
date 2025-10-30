9e12c94f25323bfd924b0fbd0b59eb9e3f7705b7
David Grudl <david@grudl.com>
2025-10-30 20:10:44 +0100

    updated rule name

--- preset-fixer/php81.php
+++ preset-fixer/php81.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 $config = require __DIR__ . '/php80.php';
 
 $rules = [
-	'@PHP81Migration' => true,
+	'@PHP8x1Migration' => true,
 ];
 
 $config->setRules($rules + $config->getRules());
